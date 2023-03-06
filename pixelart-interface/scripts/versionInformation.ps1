800d179a84f95401fdbe981d5aa638ff77c62b5d
DerEffi <info@dereffi.com>
2023-03-06 21:16:24 +0100

    Added update information

--- pixelart-interface/scripts/versionInformation.ps1
+++ pixelart-interface/scripts/versionInformation.ps1
@@ -1,10 +1,12 @@
 ﻿$basePath = Split-Path -Parent (Split-Path -Parent $PSCommandPath)
 $buildfolder = "$basePath\build"
 $versionFile = "$buildfolder\interface.version.json"
+$templateFile = "$basePath\interface.version.json"
 $envFile = "$basePath\.env"
 $env = @{}
 
-if((Test-Path -Path $versionFile) -and (Test-Path -Path $envFile)) {
+if((Test-Path -Path $templateFile) -and (Test-Path -Path $envFile)) {
+    Copy-Item $templateFile $versionFile
     Get-Content $envFile | foreach {
         $name, $value = $_.split('=')
         $env[$name] = $value
@@ -13,7 +15,7 @@ if((Test-Path -Path $versionFile) -and (Test-Path -Path $envFile)) {
     $json = Get-Content $versionFile | ConvertFrom-Json
     $json.version = $env.REACT_APP_VERSION
     $json.type = $env.REACT_APP_ENVIRONMENT
-    $json.files = get-childitem -file -path .\ -Recurse | Resolve-Path -Relative | % { ($_ -replace "\\", "/") -replace "\.\/", "/" }
+    $json.files = get-childitem -file -path .\ -Recurse | Resolve-Path -Relative | % { "$($env["REACT_APP_UPDATE_SERVER"])$(($_ -replace "\\", "/") -replace "\.\/", "/")" }
     $json | ConvertTo-Json -depth 32 | Out-File $versionFile
 } else {
     Write-Error "Version file in build folder does not exist"
